1852c0dd9c28fd98e4b79bd53b281ff043f7d5f2
Ryota Fujiwara <r.fujiwara@bil.jp>
2023-06-06 10:55:35 +0900

    :sparkles: サンプル処理追加

--- after.js
+++ after.js
@@ -1,4 +1,18 @@
 // this code will be executed after page load
 (function() {
-  console.log('after.js executed');
+  const headerNotice = () => {
+    if (window.location.host !== '{target-host.com}') {
+      return;
+    }
+    const targets = document.querySelectorAll('.l-header__name');
+    [...targets].forEach(target => {
+      const text = target.textContent;
+      if (text.indexOf('代理アカウント') !== -1) {
+        target.classList.add('chrome-ex-header-blink');
+      } else {
+        target.classList.add('chrome-ex-header-notice');
+      }
+    });
+  };
+  headerNotice();
 })();
